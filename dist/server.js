115a36cf14b087f5a2f5faae506696e4818fd7d4
Peita Lin <n6378056@gmail.com>
2017-06-13 18:46:23 +1000

    new build

---
+++ dist/server.js
@@ -0,0 +1,26 @@
+
+
+var path = require('path');
+var fs = require('fs')
+
+var Koa = require('koa');
+var Router = require('koa-router');
+var serve = require('koa-static');
+var sendFile = require('koa-send')
+
+var koa = new Koa();
+var router = new Router()
+var PORT = process.env.PORT || 9999;
+
+router.get('/', async(ctx) => {
+  await sendFile(ctx, '/index.html', { root: 'dist' })
+})
+
+koa.use(router.routes());
+// serve all files in directory:
+koa.use(serve(path.join(__dirname)));
+
+koa.listen(PORT, () => {
+  console.log(`Koa serving: ./dist/index.html at http://localhost:${PORT}`)
+});
+
