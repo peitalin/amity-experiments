bbeb8eb7e706f0d17783f7e6e95f28e546ce463e
Christian Smith <csmit175@hotmail.com>
2017-08-02 11:00:04 +1000

    Updating news hunt ui

--- src/components/NewsHunt.tsx
+++ src/components/NewsHunt.tsx
@@ -49,7 +49,7 @@ class NewsHunt extends React.Component<StateProps & DispatchProps & ReactProps, 
     this.fetchNews()
   }
 
-  fetchNews = async() => {
+  fetchNews = async () => {
     let url = 'https://newsapi.org/v1/articles?source=techcrunch&apiKey=298e76f22d7d48599566e383b9842d53'
     await fetch(url).then(res => res.json()).then(data => this.setState({ news: data }))
     console.info(this.state.news)
@@ -74,28 +74,54 @@ class NewsHunt extends React.Component<StateProps & DispatchProps & ReactProps, 
 
   render() {
     return (
+      
       <div className='news_hunt'>
-        <div className="news_hunt_container">
-        {
-          this.state.news.articles.map(( article, i ) => {
-            return (
-              <div className='news_hunt_article' key={i}>
-                <a href={article.url}>
-                  <div className='urlToImage'>
-                    <img src={article.urlToImage}/>
+        <div className='news_hunt_header'>
+          <div className='header_content'>
+            <img src={'https://s3.amazonaws.com/amity-emails-assets/Amity_Symbol+and+Logo_Horizontal_No+Padding_03.png'}/>
+            <span className='fa fa-user'></span>
+          </div>
+        </div>
+        <div className='flex_hunt'>
+          <div className='news_hunt_left_side'>
+            <h3>Filters</h3>
+            <ul>
+              <li>Hot</li>
+              <li>New</li>
+              <li>Top</li>
+            </ul>
+          </div>
+          <div className="news_hunt_container">
+            <div className="news_hunt_header">
+              <span className='title'>Today</span>
+            </div>
+            {
+              this.state.news.articles.map((article, i) => {
+                return (
+                  <div className='news_hunt_article' key={i}>
+                    <a href={article.url}>
+                      <div className='urlToImage' style={{ backgroundImage: "url(" + article.urlToImage + ")" }}>
+                      </div>
+                    </a>
+                    <div className='article_content'>
+                      <div className='article_metadata'>
+                        <h1>{article.title}</h1>
+                        {/* <h2 className=''>{ article.author }</h2> */}
+                        <h4 className=''>{article.description}</h4>
+
+                      </div>
+                      <div className='actions_hunt'>
+                        <button onClick={this.handleUpvote}><span className='fa fa-caret-up'></span>upvote</button>
+                        <button onClick={this.handleUpvote}><span className='fa fa-commenting-o'></span>Comment</button>
+                      </div>
+                    </div>
                   </div>
-                </a>
-                <div className='article_metadata'>
-                  <h1>{ article.title }</h1>
-                  <h2 className=''>{ article.author }</h2>
-                  <h2 className=''>{ article.publishedAt }</h2>
-                  <h4 className=''>{ article.description }</h4>
-                  <button onClick={this.handleUpvote}>upvote</button>
-                </div>
-              </div>
-            )
-          })
-        }
+                )
+              })
+            }
+          </div>
+          <div className='news_hunt_right_side'>
+          </div>
         </div>
       </div>
     )
@@ -117,12 +143,12 @@ mutation($prediction: Float, $userId: ID!, $geojsonId: ID!) {
 }
 `
 
-const mapStateToProps = ( state: ReduxState ): ReduxStateUser => {
+const mapStateToProps = (state: ReduxState): ReduxStateUser => {
   return {
   }
 }
 
-const mapDispatchToProps = ( dispatch ) => {
+const mapDispatchToProps = (dispatch) => {
   return {
     updateUserGQL: (userGQL: userGQL) => dispatch(
       { type: A.User.USER_GQL, payload: userGQL }
@@ -137,4 +163,4 @@ const mapDispatchToProps = ( dispatch ) => {
 export default connect<StateProps, DispatchProps, ReactProps>(
   mapStateToProps,
   mapDispatchToProps,
-  )( NewsHunt )
+)(NewsHunt)
